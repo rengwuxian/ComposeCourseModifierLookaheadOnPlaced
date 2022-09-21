cded2c81fde8e625bdfbd555cf32cddae506372d
扔物线-朱凯 <rengwuxian@gmail.com>
2022-09-21 10:19:14 +0800

    Initial commit

---
+++ app/src/main/java/com/example/composecoursemodifierlookaheadonplaced/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.composecoursemodifierlookaheadonplaced
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.LookaheadLayout
+import androidx.compose.ui.layout.OnPlacedModifier
+import androidx.compose.ui.layout.OnRemeasuredModifier
+import androidx.compose.ui.layout.onPlaced
+import com.example.composecoursemodifierlookaheadonplaced.ui.theme.ComposeCourseModifierLookaheadOnPlacedTheme
+
+class MainActivity : ComponentActivity() {
+  @OptIn(ExperimentalComposeUiApi::class)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      ComposeCourseModifierLookaheadOnPlacedTheme {
+        OnPlacedModifier
+        OnRemeasuredModifier
+        LookaheadLayout(content = {
+                                  Row(Modifier.onPlaced { lookaheadCoordinates, coordinates ->
+
+                                  }) {
+
+                                  }
+        }, measurePolicy = )
+      }
+    }
+  }
+}
+
+for est
